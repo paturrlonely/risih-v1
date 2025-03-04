1b4c4cfee92f1ba6e85e3ee4bd7d78d5c6a397a0
paturrlonely <boboiapi54321@gmail.com>
2025-03-05 06:04:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7676914987:AAHVxRRkuXKWIllsvJHMKVsAFUpInmybFDw", // Token bot Telegram
-  OWNER_ID: "6967461527", // ID pemilik bot
+  BOT_TOKEN: "7574099627:AAF4-Gyl-7-qusHy0KJoUd4h9W3WW1vp-uI", // Token bot Telegram
+  OWNER_ID: "7530314274", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670], // ID grup yang diizinkan
 };
