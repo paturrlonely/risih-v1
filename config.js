2d4800f72191fddb3dcadb5ca3c4c91c3a59705f
paturrlonely <boboiapi54321@gmail.com>
2025-03-05 06:03:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7676914987:AAHVxRRkuXKWIllsvJHMKVsAFUpInmybFDw", // Token bot Telegram
+  OWNER_ID: "6967461527", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670], // ID grup yang diizinkan
+};
